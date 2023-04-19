11f37583935955b97caecbd620c196ccf6e2b2c8
ismaelbarbersanchezheredero <123732313+ismaelbarbersanchezheredero@users.noreply.github.com>
2023-04-19 18:24:49 +0200

    Cliente P2P. Falta añadir la parte de que pueda escribir y recibir mensajes

--- peeri.py
+++ peeri.py
@@ -3,13 +3,14 @@ import sys
 import pickle
 import select
 
-ip_serv = "192.168.0.37"
+ip_serv = "192.168.0.40"
 #puerto = int(sys.argv[2])
 port_serv = 1025
 puerto = 0 #El Sist. Operativo asignará un puerto aleatorio disponible para el socket
 address_serv = (ip_serv,port_serv)
 
 entradas = [] #Creamos una lista
+nuevos_sockets = []
 clientes = {} #Creamos diccionario
 
 # Creación del socket
@@ -20,9 +21,9 @@ client_socket.connect(address_serv)
 data = client_socket.recv(1024) #Recibimos la lista de usuarios del servidor
 entradas = pickle.loads(data) #Reconstruir la lista
 
-cl_server_address = ('192.168.0.37', 0)
+cl_server_address = ('192.168.0.40', 0)
 
-# Crear un objeto socket, además de comprobar que no ocurren errores en la creación del socket.
+# Crear un objeto socket que actuara como servidor, además de comprobar que no ocurren errores en la creación del socket.
 try:
     clsocketserv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except Exception as e:
@@ -38,24 +39,34 @@ except Exception as e:
     print("Error en la asociación del socket: ", e)
 
 #Conexion con los clientes de la lista
+for sock in entradas:
+    try:
+        # Crear un nuevo socket para conectarse a los cliente
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-'''
-Rellenar
-'''
+        # Conectar al cliente. sock.getpeername()[0] extrae el primer elemento de la tupla, que es la dirección IP remota,
+        # y la expresión sock.getpeername()[1] extrae el segundo elemento de la tupla, que es el número de puerto remoto.
+        sock.connect((sock.getpeername()[0], sock.getpeername()[1]))
+        nuevos_sockets.append(sock)#Almaceno cada socket en la lista nuevos_sockets
+        # Almacenar el socket en el diccionario de clientes
+        clientes[sock] = sock.getpeername()
+    except Exception as e:
+        print("Error al conectar a", sock.getpeername(), ":", e)
 
-#Añado y coloco el socket en la primera posicion de la lista
-entradas.insert(0,clsocketserv)
+#Añado y coloco el socket servidor en la primera posicion de la lista
+nuevos_sockets.insert(0,clsocketserv)
 # Escuchar por conexiones entrantes
 clsocketserv.listen(5)
 
-while entradas != -1:
+while nuevos_sockets != -1:
 
-    readable, writable, exceptional = select.select(entradas,[],[])
+    readable, writable, exceptional = select.select(nuevos_sockets,[],[])
 
     for s in readable:
+        #Hay un cliente nuevo intentando conectarse
         if s is clsocketserv:
             conexion, client_address = s.accept()
-            entradas.append(conexion)
+            nuevos_sockets.append(conexion)
             #clientes[conexion] = client_address
             print("Conexion entrante por parte de {}:{}".format(*client_address))
 
@@ -67,7 +78,7 @@ while entradas != -1:
                 print('Conexión cerrada por {}:{}\n'.format(*client_address))
 
                 #eliminamos el primer elemento de una lista que coincida con el valor s.
-                entradas.remove(s)
+                nuevos_sockets.remove(s)
                 #Borramos la entrada correspondiente tambien del diccionario
                 if s in clientes:
                     del clientes[s]
